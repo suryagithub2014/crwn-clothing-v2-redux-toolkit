2e056eff24e0a6400383e91c6f51d9d701aad8d6
suryagithub2014 <prakash.dotnet2008@gmail.com>
2023-06-24 16:51:57 +0530

    toolkit

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -21,12 +21,12 @@ import {
 } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+  apiKey: "AIzaSyB8MtRADBPDAyiwtwOjolL5WPfHTkekDW4",
+    authDomain: "crw-cloth-db.firebaseapp.com",
+    projectId: "crw-cloth-db",
+    storageBucket: "crw-cloth-db.appspot.com",
+    messagingSenderId: "53367691503",
+    appId: "1:53367691503:web:47b949da69afc97906a31a"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
